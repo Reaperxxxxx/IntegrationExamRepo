acbda5f7c9425117ff810440bb8264f5ce815546
ghaith hammadi <ghaith.hammadi@esprit.tn>
2018-11-27 23:00:21 +0100

    adding first project

---
+++ integration-exam-ejb/src/main/java/Test.java
@@ -0,0 +1,9 @@
+import tn.esprit.ic.service.ServiceTest;
+
+public class Test {
+
+	public static void main(String [] args ){
+		ServiceTest st = new ServiceTest();
+		st.say();
+	}
+}
---
+++ integration-exam-ejb/src/main/java/tn/esprit/ic/service/ServiceTest.java
@@ -0,0 +1,8 @@
+package tn.esprit.ic.service;
+
+public class ServiceTest {
+
+	public void say(){
+		System.out.print("« Welcome to IC »");
+	}
+}
